ecb80e3cbac0ca4395c985658e4a75d9c38c3995
lintangwisesa <lintangwisesa@ymail.com>
2018-12-06 11:48:23 +0700

    react cuaca

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  state = {kota: '', cuaca:''}
   render() {
+    let cari = () => {
+      var apikey = '361fca442399d6dac58d4c36a466273b'
+      var url = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.kota}&APPID=${apikey}`
+      axios.get(url)
+      .then((z)=>{
+        console.log(z.data)
+        this.setState({cuaca: z.data})
+      })
+      .catch((z)=>{console.log(z)})
+    }
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Cuaca Hari Ini</p>
         </header>
+        <input ref='kota' type='text' 
+        placeholder='Ketik kota Anda...'
+        onInput={()=>{
+          this.setState({kota: this.refs.kota.value})}
+        }/>
+        <button onClick={cari}>Cari</button>
+        <p>{this.state.cuaca ? this.state.cuaca.name : ''}</p>
+        <p>{this.state.cuaca ? this.state.cuaca.coord.lon : ''}</p>
+        <p>{this.state.cuaca ? this.state.cuaca.coord.lat : ''}</p>
+        <p>{this.state.cuaca ? (this.state.cuaca.main.temp - 273) : ''}</p>
       </div>
     );
   }
